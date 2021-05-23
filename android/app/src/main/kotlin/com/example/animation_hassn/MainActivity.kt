6a3e59a46d784de69eaf23781e692fb286a37bbb
khaled <khaledsaleh-alali@khaleds-MacBook-Pro.local>
2021-05-23 17:19:34 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/animation_hassn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animation_hassn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(AnimatedContainerApp());
+
+class AnimatedContainerApp extends StatefulWidget {
+  @override
+  _AnimatedContainerAppState createState() => _AnimatedContainerAppState();
+}
+
+class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
+  // Define the various properties with default values. Update these properties
+  // when the user taps a FloatingActionButton.
+  bool visible = true;
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AnimatedContainer Demo'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedContainer(
+                // Use the properties stored in the State class.
+                width: _width,
+                height: _height,
+                decoration: BoxDecoration(
+                  color: _color,
+                  borderRadius: _borderRadius,
+                ),
+                // Define how long the animation should take.
+                duration: Duration(seconds: 1),
+                // Provide an optional curve to make the animation feel smoother.
+                curve: Curves.fastOutSlowIn,
+              ),
+
+              AnimatedOpacity(
+                opacity: visible==true?1:0,
+                duration: Duration(microseconds: 2),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('jhaled alali '),
+                    Text('jhaled alali '),
+                    Text('jhaled alali ')
+
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          // When the user taps the button
+          onPressed: () {
+            setState(() {
+              // Create a random number generator.
+
+              visible=!visible;
+              double opacity = visible == true?1:0;
+              final random = Random();
+
+              // Generate a random width and height.
+              _width = random.nextInt(300).toDouble();
+              _height = random.nextInt(300).toDouble();
+
+              // Generate a random color.
+              _color = Color.fromRGBO(
+                random.nextInt(256),
+                random.nextInt(256),
+                random.nextInt(256),
+                visible==true?0:1,
+              );
+
+              // Generate a random border radius.
+              _borderRadius =
+                  BorderRadius.circular(random.nextInt(100).toDouble());
+            });
+          },
+          child: Icon(Icons.flip),
+        ),
+      ),
+    );
+  }
+}
